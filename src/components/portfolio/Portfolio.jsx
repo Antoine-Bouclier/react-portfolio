30049e7f408aa9df3c00247130f67c075ce75947
Antoine-Bouclier <antoinebouclier1@gmail.com>
2023-12-11 16:02:35 +0100

    feat: Start adding the portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import './portfolio.css'
 
 const portfollio = () => {
   return (
-    <section id='portfolio'>portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <h3>This is a portfolio item title</h3>
+            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com" className='btn btn-primary'>Github</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
