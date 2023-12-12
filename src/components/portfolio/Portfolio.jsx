8e6b283bb61d7824145e5422e6bc552a82d3e2f8
Antoine-Bouclier <antoinebouclier1@gmail.com>
2023-12-12 09:54:44 +0100

    feat: Adding Style CSS to portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.png'
-import IMG2 from '../../assets/portfolio2.png'
+import IMG1 from '../../assets/portfolio1.PNG'
+import IMG2 from '../../assets/portfolio2.PNG'
 
 const portfollio = () => {
   return (
@@ -15,16 +15,20 @@ const portfollio = () => {
             <img src={IMG1} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
+            <img src={IMG2} alt="" />
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href="https://github.com" className='btn' target='_blank'>Github</a>
-          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className='btn' target='_blank'>Github</a>
+            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
       </div>
     </section>
