a7e1038180258a326ed9df7e42991852cbd34126
Antoine-Bouclier <antoinebouclier1@gmail.com>
2023-12-12 10:06:51 +0100

    feat: Generating portfolio items from Array using map method

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,6 +3,30 @@ import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.PNG'
 import IMG2 from '../../assets/portfolio2.PNG'
 
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'SeasOnly',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/Alien_pixels',
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'React Portfolio',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/Alien_pixels',
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'SeasOnly',
+    github: 'https://github.com',
+    demo: 'https://dribbble.com/Alien_pixels',
+  }
+]
+
 const portfollio = () => {
   return (
     <section id='portfolio'>
@@ -10,26 +34,22 @@ const portfollio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
-        <article className='portfolio__item'>
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn' target='_blank'>Github</a>
-            <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
-          </div>
-        </article>
+          {
+            data.map(({id, image, title, github, demo}) => {
+              return (
+                <article key={id} className='portfolio__item'>
+                  <div className="portfolio__item-image">
+                    <img src={image} alt={title} />
+                  </div>
+                  <h3>{title}</h3>
+                  <div className="portfolio__item-cta">
+                    <a href={github} className='btn' target='_blank'>Github</a>
+                    <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                  </div>
+                </article>
+              )
+            })
+          }
       </div>
     </section>
   )
