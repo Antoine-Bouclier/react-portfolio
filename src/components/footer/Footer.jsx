53f5c8e33eeb20afa3182d5abc4eafa43196bb7b
Antoine-Bouclier <antoinebouclier1@gmail.com>
2023-12-12 11:12:44 +0100

    feat: Start footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from "react-icons/bs"
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Antoine Bouclier</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/antoine-bouclier-4b206112a/"><BsLinkedin/></a>
+        <a href="https://github.com/Antoine-Bouclier"><FaGithub/></a>
+      </div>
+    </footer>
   )
 }
 
