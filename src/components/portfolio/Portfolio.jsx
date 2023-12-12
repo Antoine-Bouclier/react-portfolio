0a7ad70479f4bf9b224fbf74e918540149d3ffd0
Antoine-Bouclier <antoinebouclier1@gmail.com>
2023-12-12 09:44:19 +0100

    feat: portfolio component in progress

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.png'
+import IMG2 from '../../assets/portfolio2.png'
 
 const portfollio = () => {
   return (
@@ -10,10 +12,19 @@ const portfollio = () => {
       <div className="container portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-            <h3>This is a portfolio item title</h3>
-            <a href="https://github.com" className='btn'>Github</a>
-            <a href="https://github.com" className='btn btn-primary'>Github</a>
+            <img src={IMG1} alt="" />
           </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com" className='btn' target='_blank'>Github</a>
+          <a href="https://dribbble.com/Alien_pixels" className='btn btn-primary' target='_blank'>Live Demo</a>
         </article>
       </div>
     </section>
